2879e8a65be4bd9ea542b458f66d88a29a4f294d
zhghx <zhenghegong@hotmail.com>
2024-04-18 12:26:18 +0900

    完善蓝牙推送特征

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,19 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLE2902.h>
 
 // 定义用于按钮输入的GPIO引脚
 #define BUTTON_PIN 0
 
 // BLE服务和特性UUID
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define READ_CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define NOTIFY_CHARACTERISTIC_UUID "bc6fefd7-2a30-8a53-f76b-9937bc1ec4c1"
 
 BLEServer *pServer = nullptr;
-BLECharacteristic *pCharacteristic = nullptr;
+BLECharacteristic *pReadCharacteristic = nullptr;
+BLECharacteristic *pNotifyIndicateCharacteristic = nullptr;
 
 // 按钮的状态检测
 volatile bool buttonPressed = false;
@@ -45,14 +48,15 @@ void setup()
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
   BLEService *pService = pServer->createService(SERVICE_UUID);
-  pCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_READ |
-          BLECharacteristic::PROPERTY_WRITE |
-          BLECharacteristic::PROPERTY_NOTIFY |
-          BLECharacteristic::PROPERTY_INDICATE);
 
-  pCharacteristic->setValue("Hello World TEST 111111111");
+  // 创建主动读取的特征
+  pReadCharacteristic = pService->createCharacteristic(READ_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ);
+  // pReadCharacteristic->setValue("Hello World TEST , Read Value!");
+
+  // 创建通知的特征
+  pNotifyIndicateCharacteristic = pService->createCharacteristic(NOTIFY_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+  pNotifyIndicateCharacteristic->addDescriptor(new BLE2902());
+
   pService->start();
 
   // 开始广播
@@ -70,12 +74,12 @@ void loop()
   // 检测按钮是否按下
   if (digitalRead(BUTTON_PIN) == LOW)
   {
-    unsigned long now = millis();                              // 获取当前时间戳
-    char buf[50];                                              // 创建足够大的缓冲区来存储消息和时间戳
-    sprintf(buf, "[*] Data from ESP32 ! Time: %lu ms !", now); // 格式化字符串包含时间戳
-    pCharacteristic->setValue(buf);                            // 设置特性的值为包含时间戳的数据
-    pCharacteristic->notify();
-    Serial.println("Btn pressed ... ");
+    unsigned long nowMillis = millis(); // 获取当前时间戳
+    std::string nowStr = std::to_string(nowMillis);
+    pReadCharacteristic->setValue("[*] Data from ESP32 ! Time: " + nowStr + " ms !");
+    pNotifyIndicateCharacteristic->setValue(nowStr); // 设置特性的值为包含时间戳的数据
+    pNotifyIndicateCharacteristic->notify();
+    Serial.println("Btn pressed ... current nowMillis: " + String(nowMillis));
     delay(1000); // 间隔1秒发送一次数据
   }
 }
